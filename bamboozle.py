0b06b77bcb8253499b403d536a55b0f8bf58244e
Matt Pinder <matt_pinder13@hotmail.com>
2018-03-06 11:46:07 +0100

    First part of cleanup on new branch

--- bamboozle.py
+++ bamboozle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import subprocess
@@ -10,41 +10,42 @@ import time
 parser = argparse.ArgumentParser(description='Obtain statistics regarding percentage coverage from bam files. \
                                               The script gives percentage of positions in an assembly/contig \
                                               with coverage greater than or equal to a given threshold')
-parser.add_argument('-a', help='Gives whole-assembly coverage stats', action="store_true")
-parser.add_argument('-c', help='Gives per-contig coverage stats', action="store_true")
+parser.add_argument('-w', '--whole', help='Gives whole-assembly coverage stats', action="store_true")
+parser.add_argument('-c', '--contig', help='Gives per-contig coverage stats', action="store_true")
 #parser.add_argument('-e', help='Contigs to ignore during analysis', nargs='*', type=str)
 parser.add_argument('Input', type=str, nargs=1, help='Bam file to take stats from')
-parser.add_argument('Threshold', type=int, nargs=1, help='Threshold for calculating coverage percentage')
+parser.add_argument('-t' ,'--threshold', type=int, nargs='?', const=1, default='20', help='Threshold for calculating coverage percentage; default 20')
+parser.add_argument('--dev', help=argparse.SUPPRESS, action="store_true")
 
 args = parser.parse_args()
 
 ###################################################################################
 
 # Devel. This part should be handled by the arg parser
-if args.a == True and args.c == True:
-        print "Please select only one protocol!"
+if args.whole == True and args.contig == True:
+	print("Please select only one protocol!")
 	exit()
 
-if args.a == False and args.c == False:
-	print "Please select a protocol!"
-	print "For details, see help screen."
+if args.whole == False and args.contig == False:
+	print("Please select a protocol!")
+	print("For details, see help screen.")
 	exit()
 
 ##########
 
-# Devel. Add to the "--dev" argument
-start_time = time.time()
+if args.dev == True:
+	start_time = time.time()
 
 # Devel. Add try except in separate mdule
 samtools = str('/usr/local/packages/samtools-1.3.1/samtools')
 input_bam = args.Input[0]
-threshold = args.Threshold[0]
+threshold = args.threshold
 
 
 #if args.e[0] == True:
-#	print args.e[0]
+#	print(args.e[0])
 #	ignore_contigs = [args.e[0:]]
-#	print ignore_contigs
+#	print(ignore_contigs)
 
 
 # Generate a per-base coverage for each contig
@@ -58,10 +59,10 @@ process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 # Then sum all values in the dictionary and print something informative
 
 def whole_assembly():
-	print "Obtaining whole-genome stats for " + input_bam + "; coverage >+" + str(threshold) + "%."
+	print("Obtaining whole-genome stats for " + input_bam + "; coverage >+" + str(threshold) + "%.")
 	coverage_stats = {}
 	with process.stdout as result:
-		rows = (line.split('\t') for line in result)
+		rows = (line.decode().split('\t') for line in result)
 		num_lines = 0
 		for row in rows:
 			coverage = int(row[2])
@@ -71,16 +72,16 @@ def whole_assembly():
 				else:
 					coverage_stats[coverage] = 1
 			num_lines += 1
-	print "Length of assembly: " + str(num_lines)
+	print("Length of assembly: " + str(num_lines))
 	value = 100.0 / num_lines * sum(coverage_stats.values())
-	print str(value) + "% of the assembly has >=" + str(threshold) + "x coverage."
+	print(str(value) + "% of the assembly has >=" + str(threshold) + "x coverage.")
 
 # Should be as above, but instead save each contig as a dictionary key at its first appearance
 # Then add one each time that key appears with a value >= the given threshold
 # Finally, run the statistics for each contig and print as above
 
 def per_contig():
-	print "Obtaining per-contig stats for " + input_bam + "; coverage >+" + str(threshold) + "%."
+	print("Obtaining per-contig stats for " + input_bam + "; coverage >+" + str(threshold) + "%.")
 	max_length = {}
 	GTOET_threshold = {}
 	with process.stdout as result:
@@ -96,20 +97,20 @@ def per_contig():
 			max_length[str(row[0])] += 1
 	for key in max_length:
 		value = 100.0 / max_length[key] * GTOET_threshold[key]
-		print "Length of contig " + key + " = " + str(max_length[key])
-		print str(value) + "% of contig " + key + " has >=" + str(threshold) + "x coverage."
-		print "---"
+		print("Length of contig " + key + " = " + str(max_length[key]))
+		print(str(value) + "% of contig " + key + " has >=" + str(threshold) + "x coverage.")
+		print("---")
 
 def main():
 
-	if args.a == True:
+	if args.whole == True:
 		whole_assembly()
-	elif args.c == True:
+	elif args.contig == True:
 		per_contig()
 
 
 if __name__ == "__main__":
 	main()
 
-# Devel. Add this to the "--dev" option
-print "Time taken = " + str(time.time() - start_time) + " seconds."
+if args.dev == True:
+	print("Time taken = " + str(time.time() - start_time) + " seconds.")
