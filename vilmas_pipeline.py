4e5642d50b4eaeb26adca52790c132afb0a23526
Vilma Canfjorden <vilma.canfjorden@gmail.com>
2018-10-04 17:20:10 +0200

    Updated python script

--- vilmas_pipeline.py
+++ vilmas_pipeline.py
@@ -3,11 +3,13 @@
 import sys
 import subprocess
 import argparse
+import fnmatch
 import os 
 													        
 parser = argparse.ArgumentParser(prog="ADD-SCRIPT-NAME-HERE")
 parser.add_argument("-v", "--verbose", action="store_true", help="Be more verbose")
 parser.add_argument("-b", "--bowtie2", action="store_true", help="Run Bowtie2")
+parser.add_argument("-s", "--samtools", action="store_true", help="Run Samtools")
 
 args = parser.parse_args()
 
@@ -16,20 +18,31 @@ base = 'P8352_102.contigs'
 file1 = 'P8352_102_S1_L001_R1_001.fastq.gz'
 file2 = 'P8352_102_S1_L001_R2_001.fastq.gz'
 sam = 'P8352_102.sam'
+bam = 'P8352_102.bam'
 
-def build():
+def bowtie2():
 	cmd1 = ['bowtie2-build', ref, base]
 	process1 = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
 	for line in process1.stdout:
 		print line
 	
-	if os.path.isfile('P8352_102.contigs.rev.2.bt2'):
-		cmd2 = ['bowtie2', '--no-unal', '--very-sensitive', '-x', base, '-1', file1, '-2', file2, '-S', sam]	
-		process2 = subprocess.Popen(cmd2, stdout=subprocess.PIPE)
-		
+	for file in os.listdir('.'):
+		if fnmatch.fnmatch(file, '*.rev.2.bt2'):
+			cmd2 = ['bowtie2', '--no-unal', '--very-sensitive', '-x', base, '-1', file1, '-2', file2, '-S', sam]	
+			process2 = subprocess.Popen(cmd2, stdout=subprocess.PIPE)
+
+def samtools():
+	for file in os.listdir('.'):
+		if fnmatch.fnmatch(file, '*.sam'):
+			cmd3 = ['samtools', 'view', '-b', sam, '>', bam]
+			process3 = subprocess.Popen(cmd3, stdout=subprocess.PIPE)
+			
 def main():
-	build()
-	
+	if args.bowtie2:
+		bowtie2()
+	elif args.samtools:
+		samtools()
+
 if __name__ == "__main__":
     main()
 
