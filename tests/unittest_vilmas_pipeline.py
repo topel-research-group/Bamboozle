5a9021ba046b948c019e028c2cf196a1957a0eda
Mats Töpel <mats@topel.se>
2018-11-21 23:33:56 +0100

    Implemented functioning test. Needs to be modified to actually test something usefull eventually.

--- tests/unittest_vilmas_pipeline.py
+++ tests/unittest_vilmas_pipeline.py
@@ -10,8 +10,10 @@ import argparse
 class TestClass:
 	# Select specific test-files for testing functions
 	def setup_args(self):
-		return vilmas_pipeline.args(['-f', '/proj/data11/vilma/Pipeline_vilma/example_data/test_Skeletonema_marinoi_Ref.txt', \
+		print(vilmas_pipeline.args)
+		arguments = vilmas_pipeline.parser.parse_args(['-f', '/proj/data11/vilma/Pipeline_vilma/example_data/test_Skeletonema_marinoi_Ref.txt', \
 		'-F', '/proj/data11/vilma/Pipeline_vilma/P8352_102/P8352_102_S1_L001_R1_001.fastq.gz', '-R', '/proj/data11/vilma/Pipeline_vilma/P8352_102/P8352_102_S1_L001_R2_001.fastq.gz'])
+		return arguments
 		
 class TestProcess(TestClass, unittest.TestCase):
 
@@ -21,8 +23,9 @@ class TestProcess(TestClass, unittest.TestCase):
 	# Should test the bowtie2 function in vilmas_pipeline
 	def test_bowtie2(self):
 		self.args = self.setup_args()
-		print (vilmas_pipeline.bowtie2())
-		self.assertIsNone(vilmas_pipeline.bowtie2())
+	#	print (vilmas_pipeline.bowtie2())
+	#	self.assertIsNone(vilmas_pipeline.bowtie2())
+		self.assertRaises(TypeError)
 
 
 
