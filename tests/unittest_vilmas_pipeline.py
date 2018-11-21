afad334efc9b6d074f557c293b8efd6d7eeffa30
Mats Töpel <mats@topel.se>
2018-11-22 00:10:26 +0100

    Added a dummy test that can be changed into something usefull later on

--- tests/unittest_vilmas_pipeline.py
+++ tests/unittest_vilmas_pipeline.py
@@ -21,17 +21,20 @@ class TestClass:
 		
 class TestProcess(TestClass, unittest.TestCase):
 
+	def __init__(self):
+		self.args = self.setup_args()
+
 	def setUp(self):
 		pass
 
 	# Test the bowtie2 function in vilmas_pipeline
 	def test_bowtie2(self):
-		self.args = self.setup_args()
+#		self.args = self.setup_args()
 	#	self.assertIsNone(vilmas_pipeline.bowtie2())
 		self.assertRaises(TypeError)
 
 	def test_dummy(self):
-		self.arguments = self.setup_args()
+#		self.arguments = self.setup_args()
 		self.assertTrue(self.arguments, '/proj/data11/vilma/Pipeline_vilma/example_data/test_Skeletonema_marinoi_Ref.txt')
 
 	def tearDown(self):
