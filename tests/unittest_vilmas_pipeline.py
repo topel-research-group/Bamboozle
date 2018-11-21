4c2b54fbe4696bebff13832cfc80aaa48f8bb5af
Mats Töpel <mats@topel.se>
2018-11-22 00:15:12 +0100

    Changed from full path declaration to rellative path to infut files

--- tests/unittest_vilmas_pipeline.py
+++ tests/unittest_vilmas_pipeline.py
@@ -12,30 +12,27 @@ class TestClass:
 	def setup_args(self):
 		print(vilmas_pipeline.args)
 		arguments = vilmas_pipeline.parser.parse_args(['-f', \
-					'/proj/data11/vilma/Pipeline_vilma/example_data/test_Skeletonema_marinoi_Ref.txt', \
+					'../example_data/test_Skeletonema_marinoi_Ref.txt', \
 					'-F', \
-					'/proj/data11/vilma/Pipeline_vilma/P8352_102/P8352_102_S1_L001_R1_001.fastq.gz', \
+					'../P8352_102/P8352_102_S1_L001_R1_001.fastq.gz', \
 					'-R', \
-					'/proj/data11/vilma/Pipeline_vilma/P8352_102/P8352_102_S1_L001_R2_001.fastq.gz'])
+					'../P8352_102/P8352_102_S1_L001_R2_001.fastq.gz'])
 		return arguments
 		
 class TestProcess(TestClass, unittest.TestCase):
-
-	def __init__(self):
-		self.args = self.setup_args()
 
 	def setUp(self):
 		pass
 
 	# Test the bowtie2 function in vilmas_pipeline
 	def test_bowtie2(self):
-#		self.args = self.setup_args()
+		self.args = self.setup_args()
 	#	self.assertIsNone(vilmas_pipeline.bowtie2())
 		self.assertRaises(TypeError)
 
 	def test_dummy(self):
-#		self.arguments = self.setup_args()
-		self.assertTrue(self.arguments, '/proj/data11/vilma/Pipeline_vilma/example_data/test_Skeletonema_marinoi_Ref.txt')
+		self.arguments = self.setup_args()
+		self.assertTrue(self.arguments, '../example_data/test_Skeletonema_marinoi_Ref.txt')
 
 	def tearDown(self):
 		# Remove an empty directory
