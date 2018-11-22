e67565980951997705b14bd56f9b08c6fd2643e6
Mats Töpel <mats@topel.se>
2018-11-22 10:03:15 +0100

    Minor changes

--- tests/unittest_vilmas_pipeline.py
+++ tests/unittest_vilmas_pipeline.py
@@ -10,14 +10,14 @@ import os
 class TestClass:
 	# Select specific test-files for testing functions
 	def setup_args(self):
-		print(vilmas_pipeline.args)
-		arguments = vilmas_pipeline.parser.parse_args(['-f', \
+#		print(vilmas_pipeline.args)
+		args = vilmas_pipeline.parser.parse_args(['-f', \
 					'../example_data/test_Skeletonema_marinoi_Ref.txt', \
 					'-F', \
 					'../P8352_102/P8352_102_S1_L001_R1_001.fastq.gz', \
 					'-R', \
 					'../P8352_102/P8352_102_S1_L001_R2_001.fastq.gz'])
-		return arguments
+		return args
 		
 class TestProcess(TestClass, unittest.TestCase):
 
@@ -26,14 +26,13 @@ class TestProcess(TestClass, unittest.TestCase):
 
 	# Test the bowtie2 function in vilmas_pipeline
 	def test_bowtie2(self):
-		self.args = self.setup_args()
-	#	self.assertIsNone(vilmas_pipeline.bowtie2())
-		self.assertRaises(TypeError)
+		args = self.setup_args()
+		self.assertIsNotNone(vilmas_pipeline.bowtie2())
+#		self.assertRaises(TypeError)
 
 	def test_reference_argument(self):
-		self.arguments = self.setup_args()
-#		self.assertTrue(self.arguments, '../example_data/test_Skeletonema_marinoi_Ref.txt')
-		self.assertEqual(self.arguments.ref, '../example_data/test_Skeletonema_marinoi_Ref.txt')
+		self.args = self.setup_args()
+		self.assertEqual(self.args.ref, '../example_data/test_Skeletonema_marinoi_Ref.txt')
 
 	def tearDown(self):
 		# Remove an empty directory
