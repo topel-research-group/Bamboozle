ccaf8597b74c3bf2712871981e93e9bb04d620ed
Vilma Canfjorden <vilma.canfjorden@gmail.com>
2018-12-06 16:58:22 +0100

    add fnmatch in if statemnt

--- modules/parse_gff.py
+++ modules/parse_gff.py
@@ -2,12 +2,13 @@
 
 import gffutils
 import os
+import fnmatch
 
 # Extracts feature from given gff file, output is a gff file that is input in annoation function 
 # (snpEff -interval out.gff ...) in the pipeline if args.gff and args.feature
 def main(gff, feature=''):
-	for file in os.listdir('.'):
-		if not os.path.exists("gff.db"):
+	for file in os.listdir(os.path.dirname(gff)+'/'):
+		if not fnmatch.fnmatch(file, "gff.db"):
 			fn = gffutils.example_filename(gff)
 			db = gffutils.create_db(fn, dbfn='gff.db', force=False, keep_order=True,\
 						merge_strategy='merge', sort_attribute_values=True)
@@ -16,7 +17,7 @@ def main(gff, feature=''):
 					fout.write(str(i) + '\n')
 			fout.close()
 
-	db = gffutils.FeatureDB('gff.db')
+	db = gffutils.FeatureDB(os.path.dirname(gff)+'/gff.db')
 	with open('out.gff', 'w') as fout:
 		for i in db.features_of_type(feature, order_by='ID'):
 			fout.write(str(i) + '\n')
