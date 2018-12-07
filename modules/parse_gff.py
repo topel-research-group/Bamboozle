3809f0e5a36c25f4d162606131ad867f118b9c5a
Vilma Canfjorden <vilma.canfjorden@gmail.com>
2018-12-07 14:32:29 +0100

    file iterator works

--- modules/parse_gff.py
+++ modules/parse_gff.py
@@ -7,24 +7,25 @@ import fnmatch
 # Extracts feature from given gff file, output is a gff file that is input in annoation function 
 # (snpEff -interval out.gff ...) in the pipeline if args.gff and args.feature
 def main(gff, feature=''):
-	gff_path =  os.path.dirname(gff) + '/' 
-	for f in os.listdir(gff_path):
-		print(f)
-		if fnmatch.fnmatch(f, "gff.db"):
-			db = gffutils.FeatureDB(os.path.abspath(file))
-			with open('out.gff', 'w') as fout:
-				for i in db.features_of_type(feature, order_by='ID'):
-					fout.write(str(i) + '\n')
-			fout.close()
-		else:
-			db = gffutils.create_db(gff, dbfn='gff.db', force=False, keep_order=True,\
-						merge_strategy='merge', sort_attribute_values=True)
-
-			db = gffutils.FeatureDB('gff.db')
-			with open('out.gff', 'w') as fout:
-				for i in db.features_of_type(feature, order_by='ID'):
-					fout.write(str(i) + '\n')
-			fout.close()
+	gff_path = os.path.dirname(gff)  
+	file_list = os.listdir(gff_path)
+	for f in file_list:
+		if fnmatch.fnmatch(f, 'gff.db'):
+			print(f)
+#			db = gffutils.FeatureDB(os.path.abspath(file))
+#			with open('out.gff', 'w') as fout:
+#				for i in db.features_of_type(feature, order_by='ID'):
+#					fout.write(str(i) + '\n')
+#			fout.close()
+#		else:
+#			db = gffutils.create_db(gff, dbfn='gff.db', force=False, keep_order=True,\
+#						merge_strategy='merge', sort_attribute_values=True)
+#
+#			db = gffutils.FeatureDB('gff.db')
+#			with open('out.gff', 'w') as fout:
+#				for i in db.features_of_type(feature, order_by='ID'):
+#					fout.write(str(i) + '\n')
+#			fout.close()
 
 
 if __name__ == "__main__":
