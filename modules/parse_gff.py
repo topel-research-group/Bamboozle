a315a5034e6dfccfdee5f0215940ea2cfc2f37d6
Vilma Canfjorden <vilma.canfjorden@gmail.com>
2018-12-10 13:35:37 +0100

    small changes

--- modules/parse_gff.py
+++ modules/parse_gff.py
@@ -4,13 +4,14 @@ import gffutils
 import os
 import fnmatch
 
-# Extracts feature from given gff file, output is a gff file that is input in annoation function 
-# (snpEff -interval out.gff ...) in the pipeline if args.gff and args.feature
+# Extracts feature from given gff file, output is a gff file 
+# which will work as input in the annoation function 
+# in the pipeline if args.gff and args.feature are used.
 def main(gff, feature=''):
 	gff_path = os.path.dirname(gff)  
 	file_list = os.listdir(gff_path)
 	for f in file_list:
-		# Open db if it exists, looks for it in the same dir as the gff file
+		# Open db if it exists, looks for it in the same dir as the gff file.
 		if fnmatch.fnmatch(f, 'gff.db'):
 			db = gffutils.FeatureDB(gff_path+'/gff.db')
 			with open('out.gff', 'w') as fout:
@@ -19,7 +20,7 @@ def main(gff, feature=''):
 			fout.close()
 			break
 	else:
-		# If db doesn't exists it creates new and then open db
+		# If db does not exists a new is created.
 		db = gffutils.create_db(gff, dbfn=gff_path+'/gff.db', force=False, keep_order=True,\
 					merge_strategy='merge', sort_attribute_values=True)
 
