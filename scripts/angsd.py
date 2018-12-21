c172a6b07c8c621e07598b456fc0be2db12a44c2
Vilma Canfjorden <vilma.canfjorden@gmail.com>
2018-12-21 09:32:56 +0100

    removed # from code

--- scripts/angsd.py
+++ scripts/angsd.py
@@ -45,57 +45,57 @@ def main():
 	if not os.path.exists('ANGSD'):
 		os.makedirs('ANGSD')
 
-#	directories1 = args.pop1 + '/*/Bowtie2/*.bam'
-#	bam_list1 = glob.glob(directories1)
-#	myfile = open("bam_list1.txt","w")
-#	for n1 in bam_list1:
-#		myfile.write(add+"%s\n" % n1)
-#
-#	myfile.close()
-	
-#	directories2 = args.pop2 + '/*/Bowtie2/*.bam'
-#	bam_list2 = glob.glob(directories2)
-#	myfile2 = open("bam_list2.txt","w")
-#	for n2 in bam_list2:
-#		myfile2.write(add+"%s\n" % n2)
+	directories1 = args.pop1 + '/*/Bowtie2/*.bam'
+	bam_list1 = glob.glob(directories1)
+	myfile = open("bam_list1.txt","w")
+	for n1 in bam_list1:
+		myfile.write(add+"%s\n" % n1)
 
-#	myfile2.close()
+	myfile.close()
 	
+	directories2 = args.pop2 + '/*/Bowtie2/*.bam'
+	bam_list2 = glob.glob(directories2)
+	myfile2 = open("bam_list2.txt","w")
+	for n2 in bam_list2:
+		myfile2.write(add+"%s\n" % n2)
+
+	myfile2.close()
+
 	# Calculate per pop site allele freq.
-#	cmd1 = ['angsd', '-P', '$NSLOTS', '-b', '../bam_list1.txt', \
-#		'-anc', add+ref, \
-#		'-out', 'pop1', \
-#		'-dosaf', '1', '-gl', \
-#		'1']	
-#	process1 = subprocess.Popen(cmd1, \
-#		stdout=subprocess.PIPE, \
-#		cwd='ANGSD')
-#	while process1.wait() is None:
-#		pass
-#	process1.stdout.close()
+	cmd1 = ['angsd', '-P', '$NSLOTS', '-b', '../bam_list1.txt', \
+		'-anc', add+ref, \
+		'-out', 'pop1', \
+		'-dosaf', '1', '-gl', \
+		'1']	
+	process1 = subprocess.Popen(cmd1, \
+		stdout=subprocess.PIPE, \
+		cwd='ANGSD')
+	while process1.wait() is None:
+		pass
+	process1.stdout.close()
 
-#	cmd2 = ['angsd', '-P', '$NSLOTS', '-b', '../bam_list2.txt', \
-#		'-anc', add+ref, \
-#		'-out', 'pop2', \
-#		'-dosaf', '1', \
-#		'-gl', '1']	
-#	process2 = subprocess.Popen(cmd2, \
-#		stdout=subprocess.PIPE, \
-#		cwd='ANGSD')
-#	while process2.wait() is None:
-#		pass	
-#	process2.stdout.close()
+	cmd2 = ['angsd', '-P', '$NSLOTS', '-b', '../bam_list2.txt', \
+		'-anc', add+ref, \
+		'-out', 'pop2', \
+		'-dosaf', '1', \
+		'-gl', '1']	
+	process2 = subprocess.Popen(cmd2, \
+		stdout=subprocess.PIPE, \
+		cwd='ANGSD')
+	while process2.wait() is None:
+		pass	
+	process2.stdout.close()
 
 	# Calculate 2dsfs prior.
-#	cmd3 = ('/usr/local/packages/angsd0.918/angsd/misc/realSFS \
-#		pop1.saf.idx pop2.saf.idx -P %s > pop1.pop2.ml') % ('$NSLOTS')
-#	process3 = subprocess.Popen(cmd3, \
-#		stdout=subprocess.PIPE, \
-#		shell=True, \
-#		cwd='ANGSD')
-#	while process3.wait() is None:
-#		pass
-#	process3.stdout.close()
+	cmd3 = ('/usr/local/packages/angsd0.918/angsd/misc/realSFS \
+		pop1.saf.idx pop2.saf.idx -P %s > pop1.pop2.ml') % ('$NSLOTS')
+	process3 = subprocess.Popen(cmd3, \
+		stdout=subprocess.PIPE, \
+		shell=True, \
+		cwd='ANGSD')
+	while process3.wait() is None:
+		pass
+	process3.stdout.close()
 
 	# Index and prepare for fst analysis and easy sliding window analysis.
 	cmd4 = ['/usr/local/packages/angsd0.918/angsd/misc/realSFS', \
