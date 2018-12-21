8284a4b16db1e279a55b0ee37c53679c90501352
Vilma Canfjorden <vilma.canfjorden@gmail.com>
2018-12-21 12:11:15 +0100

    script makes a pdf summary of results

--- scripts/angsd.py
+++ scripts/angsd.py
@@ -190,6 +190,77 @@ def main():
 		pass
 	process10.stdout.close()
 
+	# Making a plot of the Fst results using pandas and matplotlib, 
+	# input is the csv file and the output is a pdf file with the plot.
+	for file in os.listdir('ANGSD'):
+		if fnmatch.fnmatch(file, 'angsd_fst_headers.csv'):
+			# Import csv file with Fst results.
+			gl = pd.read_csv('ANGSD/angsd_fst_headers.csv')
+
+			# Optimize memory usage.
+			gl_int = gl.select_dtypes(include=['int'])
+			converted_int = gl_int.apply(pd.to_numeric,downcast='unsigned')
+			gl_float = gl.select_dtypes(include=['float'])
+			converted_float = gl_float.apply(pd.to_numeric,downcast='float')
+			optimized_gl = gl.copy()
+			optimized_gl[converted_int.columns] = converted_int
+			optimized_gl[converted_float.columns] = converted_float
+
+			# Convert CHROM column from object to category.
+			gl_obj = gl.select_dtypes(include=['object']).copy()
+			chrom = gl_obj.CHROM
+			chrom_cat = chrom.astype('category')
+			converted_obj = pd.DataFrame()
+
+			# If unique values are more than 50% of the data do not 
+			# convert to category, it will not optimize memory usage.
+			for col in gl_obj.columns:
+				num_unique_values = len(gl_obj[col].unique())
+				num_total_values = len(gl_obj[col])
+				if num_unique_values / num_total_values < 0.5:
+					converted_obj.loc[:,col] = gl_obj[col].astype('category')
+				else:
+					converted_obj.loc[:,col] = gl_obj[col]
+
+			# Apply on the csv file.     
+			optimized_gl[converted_obj.columns] = converted_obj
+			dtypes_col = optimized_gl.dtypes.index
+			dtypes_type = [i.name for i in optimized_gl.dtypes.values]
+			column_types = dict(zip(dtypes_col, dtypes_type))
+			read_and_optimized = pd.read_csv('ANGSD/angsd_fst_headers.csv', \
+							 dtype=column_types)
+
+			# Rename the read and optimized csv file 
+			# from the Fst analysis to "df".
+			df = read_and_optimized
+			df['code'] = chrom_cat.cat.codes
+
+			# Make plot of data.
+			df['ind'] = range(len(df))
+			df_grouped = df.groupby(('code'))
+			fig = plt.figure(figsize=(80, 20))
+			ax = fig.add_subplot(111)
+			colors = ['green', 'turquoise', 'blue', 'purple', 'red', 'orange', 'yellow']
+			x_labels = []
+			x_labels_pos = []
+			for num, (name, group) in enumerate(df_grouped):
+				group.plot(kind='scatter', x='ind', y='FST', \
+				color=colors[num % len(colors)], ax=ax)
+				x_labels.append(name)
+				x_labels_pos.append((group['ind'].iloc[-1] \
+				- (group['ind'].iloc[-1] - group['ind'].iloc[0])/2))
+				ax.set_xticks(x_labels_pos)
+				ax.set_xticklabels(x_labels, rotation='vertical', fontsize=10)
+				ax.set_xlim([0, len(df)])
+				ax.set_ylim([0, 1])
+				ax.set_xlabel('contigs', fontsize=24)
+				ax.set_ylabel('Fst value', fontsize=24)
+				ax.set_title('Weir and Cockerham Fst', fontsize=40)
+				plt.tick_params(axis='x', length=0.01)
+
+			# Save plot as pdf. 
+			plt.savefig("ANGSD/Fst_plot.pdf")
+
 	if args.clean:
 		for textfile in os.listdir('.'):
 			if fnmatch.fnmatch(textfile, 'bam_list*.txt'):
