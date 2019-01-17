2f79eccf15890a90c5e31e47ee06d1b87541df93
Vilma Canfjorden <vilma.canfjorden@gmail.com>
2019-01-17 13:23:36 +0100

    change place where the headers-block is added

--- vilmas_pipeline.py
+++ vilmas_pipeline.py
@@ -269,7 +269,7 @@ def annotation(args):
 				pass
 			process_a.stdout.close()
 
-			cmd_b = ("sed -i '/##INFO=<ID=NMD/a##INFO=<ID=out_ID,Number=1,Type=String,Description="">\
+			cmd_b = ("sed -i '/##INFO=<ID=MQ/a##INFO=<ID=out_ID,Number=1,Type=String,Description="">\
 				\\n##INFO=<ID=out_Parent,Number=1,Type=String,Description="">\
 				\\n##INFO=<ID=out_type,Number=1,Type=String,Description="">\
 				\\n##INFO=<ID=out_source,Number=1,Type=String,Description="">' \
