4eababc2e4d9741811b82c89b015e5662558d454
Vilma Canfjorden <vilma.canfjorden@gmail.com>
2019-03-29 10:12:34 +0100

    clean up

--- vilmas_pipeline/scripts/vcftools_fst.py
+++ vilmas_pipeline/scripts/vcftools_fst.py
@@ -2,23 +2,9 @@
 
 #	Input from pipeline -> this script 
 #	-> output Fst statistics table and graph
+#	Requires relative good coverage, if the
+#	coverage is low use angsd_fst.py instead.
 #	Version: VCFtools/v0.1.13
-
-#	Copyright (C) 2018 Vilma Canfjorden. vilma.canfjorden@gmail.com
-#
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
-#
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 
 import sys
 import subprocess
@@ -26,10 +12,8 @@ import argparse
 import fnmatch
 import os
 import glob
+
 import pandas as pd
-import numpy as np
-from scipy.stats import uniform
-from scipy.stats import randint
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
