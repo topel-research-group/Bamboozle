19be06d3119f280fb14ad0a3e7b2d8e6cd95326d
Matt Pinder <matt_pinder13@hotmail.com>
2019-05-22 14:27:30 +0200

    Add license details for main script and bamparser script

--- modules/bamparser.py
+++ modules/bamparser.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+
+#	Pipeline for retrieving coverage-related statistics from BAM files.
+#
+#	Copyright (C) 2018 Matthew Pinder. matt_pinder13@hotmail.com
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import sys
 import subprocess
 import argparse
@@ -8,7 +27,7 @@ from statistics import median
 
 #######################################################################
 
-parser = argparse.ArgumentParser(prog="ADD-SCRIPT-NAME-HERE")
+parser = argparse.ArgumentParser(prog="BamParser")
 parser.add_argument("-f", "--ref", \
                         help="Reference")
 parser.add_argument("-F", "--forward", \
