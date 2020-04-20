024ec08f318a04754bf633f44874121c22485ef1
André Soares <andre@albiorix.bioenv.gu.se>
2020-04-20 11:36:13 +0200

    more details for pseudocode after gridss test completed

--- modules/sv_caller.py
+++ modules/sv_caller.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env python3
 
 #André Soares - 15/04/2020
-#Pseudocode for a script calling SVs from BAM/SAM files
+#Pseudocode for a script calling SVs and inferring their LOF potential from BAM/SAM files
+
 #very much based on Matt's(?) pipeline.py in Bamboozle/modules
 
-#Input: BAM/SAM alignments of diatom (S. marinoi and others in the future?) genomes against a reference
-#Output: 
+#Input and arguments: 
+#
+#	1) sorted BAM alignments of diatom (S. marinoi and others in the future?) genomes against a reference
+#	2) genome reference
+#	3) Pilon-corrected genome reference
+#	4) GFF gene model reference
+#	?) option to run submodules individually?
+
+#Outputs: 
+#
 #	1) VCF files of alterations in gene models, with LOF prediction.
 #	2) summary tables of alterations that should include type (deletion, insertion, inversion, duplication, other genomic rearrangmements),
 #size in bp, quality of call, LOF potential, gene product, genomic location, zygosity, ?...
 
-#1. Take in mapped genomes, verify they exist, are sorted BAM files
-#1a. Take in fasta, GFF, Pilon-corrected assembly references as arguments
-#2. Run GRIDSS, make parameters available or define ideal/minimal parameters (do they even exist?)
-#  (test run GRIDSS first, make sure outputs are immediately compatible with next step)
+
+#Steps:
+#
+#1. Take in sorted alignments, verify they exist, are sorted BAM files
+#1a. Take in fasta, GFF, Pilon-corrected assembly references as arguments (check if BAMs were aligned to those refs?)
+#2. Run GRIDSS
+#  (outputs are immediately compatible with next step BUT running on bwa-mem!!! - Bamboozle runs bowtie2! - test with gridss_bowtie2.sh!!!)
 #3. Mask VCF files of SV calls with bedtools using Pilon assembly
 #4. Run snpEff on masked SV calls to obtain predicted alterations in gene models
+#4a. (Modify output headers?)
 #5. Run snpEff snpSift to obtain summary tables
+
 
 #  NOT SURE about this license? Secondary though
 
@@ -71,7 +85,9 @@ def timing(function):
                 return result
         return wrapper
 
-# Variant calling using GRIDSS, input is sorted BAM file,
+# SV calling using GRIDSS, input is sorted BAM file,
+# - function to make sure input is as needed!
+#
 # output file is a gzipped vcf file,
 # makes new directory 'gridss' if it doesn't exist.
 @timing
@@ -99,6 +115,8 @@ def gridss(args,threads,sorted_bam_out):
 ###
 
 # BEDTOOLS masking of SV calls goes here
+
+# input and output are vcf.gz
 
 ###
 
