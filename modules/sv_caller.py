fe7e7410b0c3b89f5c242637f372aa50067d3b92
André Soares <andre@albiorix.bioenv.gu.se>
2020-05-20 16:44:19 +0200

    fixed syntax/format errors throughout

--- modules/sv_caller.py
+++ modules/sv_caller.py
@@ -118,20 +118,18 @@ def bam_check(bam):
 	if not os.path.exists('sv_caller_output'):
                 os.makedirs('sv_caller_output')
 	#command to check out first line of BAM header and look for "coordinate" (= sorted)
-	cmd1 = ("samtools view -H %s \
+	cmd1 = "samtools view -H %s \
 		| head -n1 \
 		| cut -f3 \
-		| cut -f2 -d$':'")
-		% (bam)
+		| cut -f2 -d$':'" % (bam)
 	proc_1 = subprocess.Popen(cmd1,
 		cwd='sv_caller_output')
 	#is this even the right way to rename output? won't this go like bam_name.bam_sorted.bam? bah
 	if proc.stdout.read() == "coordinate":
 		print("Input BAM is sorted")
-	else
-	        cmd2 = ("samtools sort %s \
-	                -o %s")
-		        % (bam,bam+"_sorted.bam")
+	else:
+		cmd2 = "samtools sort %s \
+        	        -o %s" % (bam,bam+"_sorted.bam")
 
 		proc_2 = subprocess.Popen(cmd2,
 			cwd='sv_caller_output')
@@ -143,9 +141,8 @@ def ref_check(reffa):
 	for file in os.listdir(os.path.dirname(reffa)):
 		if file.endswith(('.fai','.amb','.ann','.bwt','.pac','.sa')):
 			print("bwa-mem indices exist")
-		else
-			cmd3 = ("bwa index %s")
-			% (reffa)
+		else:
+			cmd3 = "bwa index %s" % (reffa)
 			proc_3 = subprocess.Popen(cmd3,
 				cwd='sv_caller_output')
 			print("bwa-mem indices didn't exist but they sure do now")
@@ -156,23 +153,22 @@ def gridss(bam, reffa, threads, java_gridss, assembly_bam_out, sorted_bam_out):
         log_file=open('pipeline.log','a')
         ref_path = add + str(args.ref)
 
-        cmd4 = ("gridss.sh \
-		%s,
-		-r %s,
-		-a %s,
-		-o %s,
-		-t %s,
-		-j %s") \
-        % (bam, reffa, assembly_bam_out, sorted_bam_out, threads, java_gridss)
+        cmd4 = "gridss.sh \
+		%s, \
+		-r %s, \
+		-a %s, \
+		-o %s, \
+		-t %s, \
+		-j %s" % (bam, reffa, assembly_bam_out, sorted_bam_out, threads, java_gridss)
 
         process4 = subprocess.Popen(cmd4, \
                 stdout=subprocess.PIPE, \
                 stderr = log_file, \
                 shell=True, \
                 cwd='sv_caller_output')
-        while process7.wait() is None:
+        while process4.wait() is None:
                 pass
-        process7.stdout.close()
+        process4.stdout.close()
         log_file.close()
 
 ###
@@ -214,133 +210,3 @@ def snpEff_test(args):
                                 call(["pip", "install", "gffutils"])
                         else:
                                 sys.exit("[Error] Exiting due to missing dependency \"gffutils\"")
-
-# Annotating variant calling output using snpEff, output is a vcf,
-# the vcf file is bgzipped to work as an input file to the Fst analysis,
-# the original vcf file is kept by using the -c flag.
-@timing
-def annotation(args):
-        log_file=open('pipeline.log','a')
-        for file in os.listdir('Bcftools'):
-                if fnmatch.fnmatch(file, '*.bcftools_filtered.vcf.gz'):
-                        my_output = annotated_vcf
-                        my_interval = ""
-                        if args.gff and args.feature:
-                                from modules.parse_gff_2 import main as parse
-                                parse(args.gff, args.feature, args.contigsizes)
-                                out = add + 'out.gff'
-                                my_interval = "-fi %s" % out
-                                my_output = name + '_' + args.feature + '_snpeff_annotated.vcf'
-
-                        # If you want to specify options yourself.
-                        if args.snpeff:
-                                opt = '-'+' -'.join(args.snpeff)
-                                my_args = my_interval + " " + opt \
-                                        + " Skeletonema_marinoi_v1.1.1.1 \
-                                         -stats snpEff_summary.html"
-                        else:
-                                my_args = my_interval + \
-                                        " Skeletonema_marinoi_v1.1.1.1 \
-                                        -stats snpEff_summary.html"
-
-                        cmd8 = ("snpEff %s %s > %s") \
-                                % (my_args, bcftools_out, my_output)
-                        process8 = subprocess.Popen(cmd8, \
-                                stdout=subprocess.PIPE, \
-                                stderr = log_file, \
-                                shell=True, \
-                                cwd='Bcftools')
-                        while process8.wait() is None:
-                                pass
-                        process8.stdout.close()
-                        cmd9 = ('bgzip -c %s > %s') \
-                                % (my_output, my_output + '.gz')
-                        process9 = subprocess.Popen(cmd9, \
-                                stdout=subprocess.PIPE, \
-                                stderr = log_file, \
-                                shell=True, \
-                                cwd='Bcftools')
-                        while process9.wait() is None:
-                                pass
-                        process9.stdout.close()
-
-                        # Add headers to gff parsed vcf file for fst statistics.
-                        if args.gff and args.feature:
-                                my_output_hdr = name + '_' + args.feature + '_hdr_snpeff_annotated.vcf'
-                                cmd_a = ("bcftools view -h %s > hdr.txt") \
-                                        % (my_output)
-                                process_a = subprocess.Popen(cmd_a, \
-                                        stdout=subprocess.PIPE, \
-                                        stderr = log_file, \
-                                        shell=True, \
-                                        cwd='Bcftools')
-                                while process_a.wait() is None:
-                                        pass
-                                process_a.stdout.close()
-
-                                cmd_b = ('''sed -i '/##INFO=<ID=MQ/a##INFO=<ID=out_ID,Number=1,Type=String,Description="none">\
-                                        \\n##INFO=<ID=out_Parent,Number=1,Type=String,Description="none">\
-                                        \\n##INFO=<ID=out_type,Number=1,Type=String,Description="none">\
-                                        \\n##INFO=<ID=out_source,Number=1,Type=String,Description="none">' \
-                                        hdr.txt''')
-                                process_b = subprocess.Popen(cmd_b, \
-                                        stdout=subprocess.PIPE, \
-                                        stderr = log_file, \
-                                        shell=True, \
-                                        cwd='Bcftools')
-                                while process_b.wait() is None:
-                                        pass
-                                process_b.stdout.close()
-                                cmd_c = ("bcftools reheader -h hdr.txt %s > %s") \
-                                        % (my_output, my_output_hdr)
-                                process_c = subprocess.Popen(cmd_c, \
-                                        stdout=subprocess.PIPE, \
-                                        stderr = log_file, \
-                                        shell=True, \
-                                        cwd='Bcftools')
-                                while process_c.wait() is None:
-                                        pass
-                                process_c.stdout.close()
-
-                                cmd_d = ('bgzip -c %s > %s') \
-                                        % (my_output_hdr, my_output_hdr + '.gz')
-                                process_d = subprocess.Popen(cmd_d, \
-                                        stdout=subprocess.PIPE, \
-                                        stderr = log_file, \
-                                        shell=True, \
-                                        cwd='Bcftools')
-                                while process_d.wait() is None:
-                                        pass
-                                process_d.stdout.close()
-                                os.remove('out.gff')
-                                os.remove('Bcftools/hdr.txt')
-                                for vcffile in os.listdir('Bcftools'):
-                                        if fnmatch.fnmatch(vcffile, '*'+args.feature+'_snpeff_annotated.vcf'+'*'):
-                                                os.remove('Bcftools/' + vcffile)
-                        else:
-                                pass
-        log_file.close()
-
-
-# Filtering and making a summary of annotated files using
-# the vcf (not bgzipped) output file from snpEff,
-# the summary will be in table format, tab separated.
-@timing
-def snpsift(args):
-        for file in os.listdir('Bcftools'):
-                if fnmatch.fnmatch(file, '*_annotated.vcf'):
-                        my_output = annotated_vcf
-                        if args.gff and args.feature:
-                                my_output = name + '_' + args.feature + '_hdr_snpeff_annotated.vcf'
-
-                        cmd10 = ('java -jar /usr/local/packages/snpEff/SnpSift.jar \
-                        extractFields -e "." -s "," %s CHROM POS "EFF[*].GENE" REF ALT QUAL DP AF \
-                        "EFF[*].EFFECT" "EFF[*].AA" "EFF[*].FUNCLASS" > %s') \
-                        % (my_output, annotated_table)
-                        process10 = subprocess.Popen(cmd10, \
-                                stdout=subprocess.PIPE, \
-                                shell=True, \
-                                cwd='Bcftools')
-                        while process10.wait() is None:
-                                pass
-                        process10.stdout.close()
