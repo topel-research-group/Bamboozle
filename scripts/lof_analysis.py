a40a002bc2e9c44f905cf14daf250c87b1737d1a
GeoMicroSoares <andre@albiorix.bioenv.gu.se>
2020-06-25 09:50:38 +0200

    pseudocode for lof analysis

--- scripts/lof_analysis.py
+++ scripts/lof_analysis.py
@@ -1,6 +1,6 @@
 #André Soares - June 2020
 
-#Pseudocode to take in multiple .vcf inputs and metadata associated to them and plot:
+#Pseudocode/notes to write code to take in multiple .vcf inputs and metadata associated to them and plot useful stuff:
 #	0. check metadata table for requirements
 #		a. check that vcf file sample name matches metadata table column
 #		b. check if other columns exist
@@ -12,3 +12,29 @@
 #		a. number of SV types, avg length, per sample, per chromosome
 #	4. produce plots of SV types per sample (w metadata?)
 #	5. produce circos plot against ref genome?
+
+#	How about an interactive shiny app? or html being made available with all this in it?
+#		Sample-specific analysis via circos, other plots of interesting stuff
+#		General stats
+#	Could be a server-based solution, if circos plots or other results hard to generate
+#	
+#	OUTPUTS:
+#		1. TABLE
+#			a.
+#		2. HTML OUTPUT
+#		PLOT 1 - SV type length violin/point plot:
+#		 - OPTION to see by sample and/or by group
+#		 - OPTION to generate sample- or faceted group-level plot
+#			a. x = chr
+#			b. y = length
+#			c. colour = sv_type
+#			d. face
+#		PLOT 2 - Circos plot:
+#		 - OPTION to see by sample
+#		 - OPTION to see x many groups faceted
+#			L1: chromosome
+#			L2: indels (<=50bp), points?
+#			L3: SVs (>50bp), bars?
+#			L4: coverage
+#			L5: 
+#			Tracks:
