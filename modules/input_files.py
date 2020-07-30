bce3e6044b0173bab67e51c0d370a43893b656f8
GeoMicroSoares <massacomgrao@gmail.com>
2020-07-30 18:20:05 +0200

    included for loops to facilitate interaction with args.sortbam being a list

---
+++ modules/input_files.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python3
+
+
+#       Bamboozle v1.1
+#       Pipeline that performs bioinformatic analysis including SNP calling 
+#       and effect prediction of fastq files or BAM file. 
+#
+#       Copyright (C) 2018 Vilma Canfjorden. vilma.canfjorden@gmail.com
+#       Copyright (C) 2018 Matthew Pinder. matt_pinder13@hotmail.com
+#       Copyright (C) 2020 André Soares. andre.soares@bioenv.gu.se
+#
+#       This program is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#######################################################################
+# IMPORTS
+#######################################################################
+
+import sys
+import os
+import subprocess
+import fnmatch
+from functools import reduce, wraps
+from time import time
+import datetime
+
+#######################################################################
+# GLOBAL VARIABLES
+#######################################################################
+
+current_directory = os.getcwd()
+name = os.path.basename(current_directory)
+add = '../'
+add2 = '../Bowtie2/'
+base = name + '.contigs'
+sam = name + '.sam'
+bam = name + '.bam'
+
+sorted_bam_out = add2 + name + '_sorted.bam'
+sorted_bam_bai = add2 + name + '_sorted.bam.bai'
+
+#######################################################################
+# HANDLING MULTIPLE INPUT FILES
+#	If input is given in a comma-separated list, convert it to
+#	a list rather than a string
+#######################################################################
+
+# DevNote - this appears not to work currently...
+
+def list_format(args):
+	for name in ["args.forward", "args.reverse", "args.bamfile"]:
+		if name and "," in name:
+			sys.exit("[Error] FASTQ/BAM input must be space-separated, not comma-separated")
+
+#######################################################################
+# HANDLING BAM FILES
+#	First, ensure that all BAM input files are sorted
+#	Otherwise, sort them
+#	Then, assign all sorted BAMs to args.sortbam
+#	Finally, ensure that if multiple BAMs are specified,
+#		the barcode command is being run
+#		Else warn the user and exit
+#######################################################################
+
+# Extracting sample name from input BAM, checking if sorted or not
+
+# DevNote - ensure that there is also a .bai file present
+
+# DevNote - combine this with the samtools sort/index commands below, rather than having the same function run twice
+
+def bam_check(args):
+	args.sortbam = []
+	for bamfile in args.bamfile:
+		bam_name = os.path.basename(bamfile[:-4])
+		bam_sorted = "Bowtie2/%s_sorted.bam" % (bam_name)
+		bam_index = "Bowtie2/%s_sorted.bai" % (bam_name)
+
+		#command to check out first line of BAM header and look for "coordinate" (= sorted)
+		cmd1 = ["samtools", "view", "-H", bamfile]
+		proc_1 = subprocess.Popen(cmd1, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE,universal_newlines=True)
+
+		#if coordinate is present in bam header, bam is sorted
+		std_out, std_error = proc_1.communicate()
+		if "coordinate" in std_out.split("\n")[0]:
+			if args.verbose:
+				print("Input BAM " + bamfile + " is already sorted")
+			args.sortbam.append(bamfile)
+		else:
+			print("Input BAM " + bamfile + " is unsorted. Sorting...")
+
+			# Makes new directory 'Bowtie2' if it doesn't exists.
+			if not os.path.exists('Bowtie2'):
+				os.makedirs('Bowtie2')
+
+			cmd2 = ["samtools", "sort", "-@", str(args.threads), bamfile, "-o", bam_sorted]
+			proc_2 = subprocess.Popen(cmd2, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+			std_out, std_error = proc_2.communicate()
+			cmd3 = ["samtools", "index", bam_sorted, bam_index]
+			proc_3 = subprocess.Popen(cmd3, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+			std_out, std_error = proc_3.communicate()
+			print("Input BAM " + bamfile + " has been sorted")
+			args.sortbam.append(bam_sorted)
+
+	if len(args.sortbam) == 1:
+		args.sortbam = args.sortbam[0]
+
+	return(args.sortbam)
+
+#######################################################################
+# TIME DECORATOR
+#######################################################################
+
+def timing(function):
+	@wraps(function)
+	def wrapper(*args, **kwargs):
+		now = datetime.datetime.now()
+		start = time()
+		result = function(*args, **kwargs)
+		end = time()
+		fh = open("time.log", "a")
+		lines_of_text = now.strftime("%Y-%m-%d %H:%M") \
+			+ ' Function: ' \
+			+ function.__name__ \
+			+ ' Elapsed time: {}'.format(end-start) \
+			+ ' seconds \n'
+		fh.writelines(lines_of_text)
+		fh.close()
+		return result
+	return wrapper
+
+#######################################################################
+# BOWTIE2
+#	Running bowtie2-build to index reference genome and bowtie2 to align.
+#######################################################################
+
+@timing
+def bowtie2(args):
+	try:
+		subprocess.check_output(['bowtie2', '--help'])
+	except FileNotFoundError:
+		sys.exit("[Error] Please ensure that Bowtie2 is in your path.")
+
+	# Ensure that a reference is provided
+	if not args.ref:
+		sys.exit("[Error] A reference fasta (--ref) must be provided.")
+
+	# Makes new directory 'Bowtie2' if it doesn't exists.
+	if not os.path.exists('Bowtie2'):
+		os.makedirs('Bowtie2')
+
+	log_file=open('pipeline.log','a')
+	# Selected input files using forward and reverse flags,
+	# the flags can take several input files.
+
+	file1 = ''
+	file2 = ''
+	if args.forward:
+		if isinstance(args.forward, str):
+			file1 = str(add + args.forward)
+		else:
+			f1 = []
+			for name in args.forward:
+				f1.append(add+name)
+			file1 += ','.join(map(str, f1))
+
+	if args.reverse:
+		if isinstance(args.reverse, str):
+			file2 = str(add + args.reverse)
+		else:
+			f2 = []
+			for name2 in args.reverse:
+				f2.append(add+name2)
+			file2 += ','.join(map(str, f2))
+
+	# Bowtie2-build, inputs are reference in fasta format and
+	# base name for index files, the output are the index files.
+
+	cmd1 = ['bowtie2-build', add+args.ref, base]
+	process1 = subprocess.Popen(cmd1, \
+		stdout=subprocess.PIPE, \
+		stderr = log_file, \
+		cwd='Bowtie2')
+	while process1.wait() is None:
+		pass
+	process1.stdout.close()
+
+	# Bowtie2 align step, input are the index files from Bowtie2-build,
+	# fastq files (forward and reverse) the output is a SAM file.
+	for file in os.listdir('Bowtie2'):
+		if fnmatch.fnmatch(file, '*.rev.1.bt2'):
+
+			cmd2 = ['bowtie2', \
+				'-p', str(args.threads), \
+				'--no-unal', \
+				'--very-sensitive', \
+				'-x', base, \
+				'-1', file1, \
+				'-2', file2, \
+				'-S', sam]
+			process2 = subprocess.Popen(cmd2, \
+				stdout=subprocess.PIPE, \
+				stderr = log_file, \
+				cwd='Bowtie2')
+			while process2.wait() is None:
+				pass
+			process2.stdout.close()
+	log_file.close()
+
+#######################################################################
+# SAMTOOLS VIEW
+#	Converting SAM to BAM using samtools view.
+#######################################################################
+
+@timing
+def samtools_view(args):
+	log_file=open('pipeline.log','a')
+	for file in os.listdir('Bowtie2'):
+		if fnmatch.fnmatch(file, '*.sam'):
+			cmd3 = ["samtools", "view", "-@", str(args.threads), "-b", "-o", bam, sam]
+			process3 = subprocess.Popen(cmd3, \
+				stdout=subprocess.PIPE, \
+				stderr = log_file, \
+				shell=False, \
+				cwd='Bowtie2')
+			while process3.wait() is None:
+				pass
+			process3.stdout.close()
+	log_file.close()
+
+#######################################################################
+# SAMTOOLS SORT - FROM BOWTIE PIPELINE
+#	Sort BAM files.
+#######################################################################
+
+@timing
+def samtools_sort(args):
+	log_file=open('pipeline.log','a')
+	for file in os.listdir('Bowtie2'):
+		if fnmatch.fnmatch(file, '*.bam'):
+			cmd4 = ['samtools', 'sort', \
+				'-@', str(args.threads), \
+				bam, \
+				'-o', sorted_bam_out]
+			process4 = subprocess.Popen(cmd4, \
+				stdout=subprocess.PIPE, \
+				stderr = log_file, \
+				cwd='Bowtie2')
+			while process4.wait() is None:
+				pass
+			process4.stdout.close()
+		args.sortbam = str('Bowtie2/' + name + '_sorted.bam')
+	log_file.close()
+
+#######################################################################
+# SAMTOOLS INDEX
+#	Index sorted BAM files.
+#######################################################################
+
+@timing
+def samtools_index(args):
+	log_file=open('pipeline.log','a')
+	for file in os.listdir('Bowtie2'):
+		if fnmatch.fnmatch(file, '*_sorted.bam'):
+			cmd6 = ['samtools','index', \
+				'-@', str(args.threads), \
+				sorted_bam_out, \
+				sorted_bam_bai]
+			process6 = subprocess.Popen(cmd6, \
+				stdout=subprocess.PIPE, \
+				stderr = log_file, \
+				cwd='Bowtie2')
+			while process6.wait() is None:
+				pass
+			process6.stdout.close()
+	log_file.close()
+
+#######################################################################
+# MAIN
+#	Check the input arguments:
+#		If FASTQ - map, sort and index
+#		If BAM - sort and index if unsorted
+#	Then add to and return args.sortbam
+#######################################################################
+
+def main(args):
+	# Ensure list inputs are in the correct format
+	list_format(args)
+
+	# If FASTQ input is given, generate a sorted BAM file
+	if args.forward and args.reverse:
+		if args.command == "barcode":
+			sys.exit("[Error] BarcodeSearch currently doesn't accept FASTQ input; please give BAM input instead.")
+		else:
+			bowtie2(args)
+			samtools_view(args)
+			samtools_sort(args)
+			samtools_index(args)
+
+	# If BAM input is given, ensure it's sorted; otherwise sort and index it
+	elif args.bamfile:
+		if len(args.bamfile) > 1 and args.command != "barcode":
+			sys.exit("[Error] Please note that only BarcodeSearch currently accepts multiple BAM inputs.")
+		elif len(args.bamfile) == 1 and args.command == "barcode":
+			sys.exit("[Error] Please note that BarcodeSearch requires multiple BAM inputs.")
+		else:
+			bam_check(args)
+
+	return(args.sortbam)
+
+#######################################################################
+
+if __name__ == "__main__":
+	main()
--- modules/sv_caller.py
+++ modules/sv_caller.py
@@ -58,106 +58,106 @@ def ref_check(reference, ref_dict):
 		print("GATK refernce index didn't exist but they sure do now")
 
 #GATK
-def run_gatk(bam_in, bam_rg, bam_dup, bam_fm, vcf_out, bam_name, reference, java_picard):
+def run_gatk(args.sortbam, reference, java_picard):
+	for bamfile in args.sortbam:
+	#generate variables for the function
+		bam_name = os.path.basename(bamfile[:-4])
+		bam_in = bamfile
+		bam_rg = "sv_caller_output/%s_rdgrp.bam" % (bam_name)
+	        bam_dup = "sv_caller_output/%s_rdgrp_nodups.bam" % (bam_name)
+        	bam_fm = "sv_caller_output/%s_rdgrp_nodups_fixmate.bam" % (bam_name)
+	        vcf_out = "sv_caller_output/%s_svcalls.vcf" % (bam_name)
 	#format input BAMs to make GATK happy
-	cmd4a = ['java','-jar',java_picard,'AddOrReplaceReadGroups',\
-		'I=', bam_in, 'O=', bam_rg,\
-		'SORT_ORDER=coordinate','RGID=foo','RGLB=bar',\
-		'RGPL=illumina','RGSM='+bam_name,\
-		'RGPU=bc1','CREATE_INDEX=True']
-	print(cmd4a)
-	proc_4a = subprocess.Popen(cmd4a, shell=False)
-	std_out, std_error = proc_4a.communicate()
-
+		cmd4a = ['java','-jar',java_picard,'AddOrReplaceReadGroups',\
+			'I=', bam_in, 'O=', bam_rg,\
+			'SORT_ORDER=coordinate','RGID=foo','RGLB=bar',\
+			'RGPL=illumina','RGSM='+bam_name,\
+			'RGPU=bc1','CREATE_INDEX=True']
+		proc_4a = subprocess.Popen(cmd4a, shell=False)
+		std_out, std_error = proc_4a.communicate()
 	#mark duplicate reads in input BAMs
-	cmd4b = ['java','-jar',java_picard,'MarkDuplicates',\
-		'I='+bam_rg,'O=', bam_dup,\
-		'M=sv_caller_output/marked_dup_metrics.txt']
-	proc_4b = subprocess.Popen(cmd4b, \
-		shell=False)
-	std_out, std_error = proc_4b.communicate()
-
+		cmd4b = ['java','-jar',java_picard,'MarkDuplicates',\
+			'I='+bam_rg,'O=', bam_dup,\
+			'M=sv_caller_output/marked_dup_metrics.txt']
+		proc_4b = subprocess.Popen(cmd4b, \
+			shell=False)
+		std_out, std_error = proc_4b.communicate()
 	#"ensures that all mate-pair information is in sync between each read and its mate pair"
-	cmd4c = ['java','-jar',java_picard,'FixMateInformation',\
-		'I=', bam_dup,'O=', bam_fm,\
-		'ADD_MATE_CIGAR=true']
-	proc_4c = subprocess.Popen(cmd4c, \
-		shell=False)
-	std_out, std_error = proc_4c.communicate()
-
+		cmd4c = ['java','-jar',java_picard,'FixMateInformation',\
+			'I=', bam_dup,'O=', bam_fm,\
+			'ADD_MATE_CIGAR=true']
+		proc_4c = subprocess.Popen(cmd4c, \
+			shell=False)
+		std_out, std_error = proc_4c.communicate()
 	#index output bam
-	cmd4d = ['samtools', 'index', bam_fm]
-	proc_4d = subprocess.Popen(cmd4d,
-		shell=False)
-	std_out, std_error = proc_4d.communicate()
-
+		cmd4d = ['samtools', 'index', bam_fm]
+		proc_4d = subprocess.Popen(cmd4d,
+			shell=False)
+		std_out, std_error = proc_4d.communicate()
 	#validate output bam
-	cmd4e = ['ValidateSamFile', '-I', bam_fm, '-MODE', 'SUMMARY']
-	proc_4d = subprocess.Popen(cmd4d,
-		shell=False)
-	std_out, std_error = proc_4d.communicate()
-
+		cmd4e = ['ValidateSamFile', '-I', bam_fm, '-MODE', 'SUMMARY']
+		proc_4d = subprocess.Popen(cmd4d,
+			shell=False)
+		std_out, std_error = proc_4d.communicate()
 	#run haplotype caller
-	cmd4f = ['gatk', '--java-options', '"-Xmx4g"', 'HaplotypeCaller',\
-		'-G', 'StandardAnnotation',\
-		'-G', 'AS_StandardAnnotation',\
-		'-G', 'StandardHCAnnotation',\
-		'-R', reference,\
-		'-I', bam_fm,\
-		'-O', vcf_out]
+		cmd4f = ['gatk', '--java-options', '"-Xmx4g"', 'HaplotypeCaller',\
+			'-G', 'StandardAnnotation',\
+			'-G', 'AS_StandardAnnotation',\
+			'-G', 'StandardHCAnnotation',\
+			'-R', reference,\
+			'-I', bam_fm,\
+			'-O', vcf_out]
+		proc_4f = subprocess.Popen(cmd4f,
+			shell=False)
+		std_out, std_error = proc_4f.communicate()
 
-	proc_4f = subprocess.Popen(cmd4f,
-		shell=False)
-	std_out, std_error = proc_4f.communicate()
+def masking(args.sortbam, refpil):
+	for bamfile in args.sortbam:
+		#generating variables for function
+		bam_name = os.path.basename(bamfile[:-4])
+		vcf_out = "sv_caller_output/%s_svcalls.vcf" % (bam_name)
+        	masked_vcf_out = "sv_caller_output/%s_sorted_masked.vcf" % (bam_name)
 
-# BEDTOOLS masking of SV calls goes here
-
-def masking(vcf_out, refpil, masked_vcf_out):
-	cmd5 = ['bedtools', 'intersect', '-v','-b', \
-		refpil, \
-		'-a', vcf_out, \
-		'-sorted', '-header']
-	with open(masked_vcf_out, "w+") as f:
-		proc_5 = subprocess.Popen(cmd5, stdout=f, shell=False)
-	std_error = proc_5.communicate()
+		cmd5 = ['bedtools', 'intersect', '-v','-b', \
+			refpil, \
+			'-a', vcf_out, \
+			'-sorted', '-header']
+		with open(masked_vcf_out, "w+") as f:
+			proc_5 = subprocess.Popen(cmd5, stdout=f, shell=False)
+		std_error = proc_5.communicate()
 
 # Checks for dependencies required for snpEff.
-def snpeff(snpeffdb1, masked_vcf_out, bamboozledir1, masked_vcf_out_lof_csv, masked_vcf_out_lof_ann):
-	cmd7 = ['snpEff', 'eff', snpeffdb1.replace("'", ""),\
-		 masked_ann_vcf_out, \
-		'-c', bamboozledir1+'/data/snpeff/snpEff.config', \
-		'-csvStats', masked_vcf_out_lof_csv]
-	with open(masked_vcf_out_lof_ann, "w+") as f:
-		proc_7 = subprocess.Popen(cmd7, stdout=f, shell=False)
-	std_error = proc_7.communicate()
+def snpeff(snpeffdb1, args.sortbam, bamboozledir1):
+	for bamfile in args.sortbam:
+	#generating variables for function
+		bam_name = os.path.basename(bamfile[:-4])
+		masked_vcf_out = "sv_caller_output/%s_sorted_masked.vcf" % (bam_name)
+		masked_vcf_out_lof_csv = "sv_caller_output/%s_sorted_masked_lof.csv" % (bam_name)
+		masked_vcf_out_lof_ann = "sv_caller_output/%s_sorted_masked_lof.vcf" % (bam_name)
 
-def main(args, bam_name):
+		cmd7 = ['snpEff', 'eff', snpeffdb1.replace("'", ""),\
+			 masked_ann_vcf_out, \
+			'-c', bamboozledir1+'/data/snpeff/snpEff.config', \
+			'-csvStats', masked_vcf_out_lof_csv]
+		with open(masked_vcf_out_lof_ann, "w+") as f:
+			proc_7 = subprocess.Popen(cmd7, stdout=f, shell=False)
+		std_error = proc_7.communicate()
+
+def main(args):
 	#picard java
 	java_picard="/usr/local/packages/picard-tools-2.18.26/picard.jar"
 	#ref_dict
 	ref_dict = "sv_caller_output/"+args.ref.replace(".fasta",".dict")
-	#outputs for gatk
-	bam_rg = "sv_caller_output/%s_rdgrp.bam" % (bam_name)
-	bam_dup = "sv_caller_output/%s_rdgrp_nodups.bam" % (bam_name)
-	bam_fm = "sv_caller_output/%s_rdgrp_nodups_fixmate.bam" % (bam_name)
-	vcf_out = "sv_caller_output/%s_svcalls.vcf" % (bam_name)
-	#outputs for bedtools
-	masked_vcf_out = "sv_caller_output/%s_sorted_masked.vcf" % (bam_name)
-	#outputs for snpeff
-	masked_vcf_out_lof_csv = "sv_caller_output/%s_sorted_masked_lof.csv" % (bam_name)
-	masked_vcf_out_lof_ann = "sv_caller_output/%s_sorted_masked_lof.vcf" % (bam_name)
 	#clean database variable
 	snpeff_db = str(args.snpeffdb).strip('[]')
 	
+
 	#calling functions for sv_caller
 	ref_check(args.ref, ref_dict)
-	print(args.sortbam)
-	run_gatk(args.sortbam, bam_rg, bam_dup, bam_fm, vcf_out, bam_name, args.ref, java_picard)
+	run_gatk(args.sortbam, args.ref, java_picard)
 	#only apply masking() if it's been called
 	if args.masking:
-		masking(vcf_out, args.masking, masked_vcf_out)
-		annotate(masked_vcf_out, bam_name, args.bamboozledir)
-		snpeff(snpeff_db, masked_ann_vcf_out, args.bamboozledir,masked_vcf_out_lof_csv, masked_vcf_out_lof_ann)
+		masking(args.sortbam, args.masking)
+		snpeff(snpeff_db, args.sortbam, args.bamboozledir)
 	else:
-		annotate(vcf_out, bam_name, args.bamboozledir)
-		snpeff(snpeff_db, masked_ann_vcf_out, args.bamboozledir, masked_vcf_out_lof_csv, masked_vcf_out_lof_ann)
+		snpeff(snpeff_db, args.sortbam, args.bamboozledir)
