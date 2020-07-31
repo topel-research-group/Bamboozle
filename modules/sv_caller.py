6807c122edcac24014e57a84180245f3b2d29a4c
GeoMicroSoares <massacomgrao@gmail.com>
2020-07-31 10:40:32 +0200

    changing to conform if args.sortbam as list

--- modules/sv_caller.py
+++ modules/sv_caller.py
@@ -58,92 +58,89 @@ def ref_check(reference, ref_dict):
 		print("GATK refernce index didn't exist but they sure do now")
 
 #GATK
-def run_gatk(sortbam, reference, java_picard):
-	for bamfile in sortbam:
+def run_gatk(bamfile, reference, java_picard):
 	#generate variables for the function
-		bam_name = os.path.basename(bamfile[:-4])
-		bam_in = bamfile
-		bam_rg = "sv_caller_output/%s_rdgrp.bam" % (bam_name)
-		bam_dup = "sv_caller_output/%s_rdgrp_nodups.bam" % (bam_name)
-		bam_fm = "sv_caller_output/%s_rdgrp_nodups_fixmate.bam" % (bam_name)
-		vcf_out = "sv_caller_output/%s_svcalls.vcf" % (bam_name)
+	bam_name = os.path.basename(bamfile[:-4])
+	bam_in = bamfile
+	bam_rg = "sv_caller_output/%s_rdgrp.bam" % (bam_name)
+	bam_dup = "sv_caller_output/%s_rdgrp_nodups.bam" % (bam_name)
+	bam_fm = "sv_caller_output/%s_rdgrp_nodups_fixmate.bam" % (bam_name)
+	vcf_out = "sv_caller_output/%s_svcalls.vcf" % (bam_name)
 	#format input BAMs to make GATK happy
-		cmd4a = ['java','-jar',java_picard,'AddOrReplaceReadGroups',\
-			'I=', bam_in, 'O=', bam_rg,\
-			'SORT_ORDER=coordinate','RGID=foo','RGLB=bar',\
-			'RGPL=illumina','RGSM='+bam_name,\
-			'RGPU=bc1','CREATE_INDEX=True']
-		proc_4a = subprocess.Popen(cmd4a, shell=False)
-		std_out, std_error = proc_4a.communicate()
+	cmd4a = ['java','-jar',java_picard,'AddOrReplaceReadGroups',\
+		'I=', bam_in, 'O=', bam_rg,\
+		'SORT_ORDER=coordinate','RGID=foo','RGLB=bar',\
+		'RGPL=illumina','RGSM='+bam_name,\
+		'RGPU=bc1','CREATE_INDEX=True']
+	proc_4a = subprocess.Popen(cmd4a, shell=False)
+	std_out, std_error = proc_4a.communicate()
 	#mark duplicate reads in input BAMs
-		cmd4b = ['java','-jar',java_picard,'MarkDuplicates',\
-			'I='+bam_rg,'O=', bam_dup,\
-			'M=sv_caller_output/marked_dup_metrics.txt']
-		proc_4b = subprocess.Popen(cmd4b, \
-			shell=False)
-		std_out, std_error = proc_4b.communicate()
+	cmd4b = ['java','-jar',java_picard,'MarkDuplicates',\
+		'I='+bam_rg,'O=', bam_dup,\
+		'M=sv_caller_output/marked_dup_metrics.txt']
+	proc_4b = subprocess.Popen(cmd4b, \
+		shell=False)
+	std_out, std_error = proc_4b.communicate()
 	#"ensures that all mate-pair information is in sync between each read and its mate pair"
-		cmd4c = ['java','-jar',java_picard,'FixMateInformation',\
-			'I=', bam_dup,'O=', bam_fm,\
-			'ADD_MATE_CIGAR=true']
-		proc_4c = subprocess.Popen(cmd4c, \
-			shell=False)
-		std_out, std_error = proc_4c.communicate()
+	cmd4c = ['java','-jar',java_picard,'FixMateInformation',\
+		'I=', bam_dup,'O=', bam_fm,\
+		'ADD_MATE_CIGAR=true']
+	proc_4c = subprocess.Popen(cmd4c, \
+		shell=False)
+	std_out, std_error = proc_4c.communicate()
 	#index output bam
-		cmd4d = ['samtools', 'index', bam_fm]
-		proc_4d = subprocess.Popen(cmd4d,
-			shell=False)
-		std_out, std_error = proc_4d.communicate()
+	cmd4d = ['samtools', 'index', bam_fm]
+	proc_4d = subprocess.Popen(cmd4d,
+		shell=False)
+	std_out, std_error = proc_4d.communicate()
 	#validate output bam
-		cmd4e = ['ValidateSamFile', '-I', bam_fm, '-MODE', 'SUMMARY']
-		proc_4d = subprocess.Popen(cmd4d,
-			shell=False)
-		std_out, std_error = proc_4d.communicate()
+	cmd4e = ['ValidateSamFile', '-I', bam_fm, '-MODE', 'SUMMARY']
+	proc_4d = subprocess.Popen(cmd4d,
+		shell=False)
+	std_out, std_error = proc_4d.communicate()
 	#run haplotype caller
-		cmd4f = ['gatk', '--java-options', '"-Xmx4g"', 'HaplotypeCaller',\
-			'-G', 'StandardAnnotation',\
-			'-G', 'AS_StandardAnnotation',\
-			'-G', 'StandardHCAnnotation',\
-			'-R', reference,\
-			'-I', bam_fm,\
-			'-O', vcf_out]
-		proc_4f = subprocess.Popen(cmd4f,
-			shell=False)
-		std_out, std_error = proc_4f.communicate()
+	cmd4f = ['gatk', '--java-options', '"-Xmx4g"', 'HaplotypeCaller',\
+		'-G', 'StandardAnnotation',\
+		'-G', 'AS_StandardAnnotation',\
+		'-G', 'StandardHCAnnotation',\
+		'-R', reference,\
+		'-I', bam_fm,\
+		'-O', vcf_out]
+	proc_4f = subprocess.Popen(cmd4f,
+		shell=False)
+	std_out, std_error = proc_4f.communicate()
 
 #masks the output for each called vcf with previous calls using reads generated to correct the assembly
 
-def masking(sortbam, refpil):
-	for bamfile in sortbam:
-		#generating variables for function
-		bam_name = os.path.basename(bamfile[:-4])
-		vcf_out = "sv_caller_output/%s_svcalls.vcf" % (bam_name)
-		masked_vcf_out = "sv_caller_output/%s_sorted_masked.vcf" % (bam_name)
+def masking(bamfile, refpil):
+	#generating variables for function
+	bam_name = os.path.basename(bamfile[:-4])
+	vcf_out = "sv_caller_output/%s_svcalls.vcf" % (bam_name)
+	masked_vcf_out = "sv_caller_output/%s_sorted_masked.vcf" % (bam_name)
 
-		cmd5 = ['bedtools', 'intersect', '-v','-b', \
-			refpil, \
-			'-a', vcf_out, \
-			'-sorted', '-header']
-		with open(masked_vcf_out, "w+") as f:
-			proc_5 = subprocess.Popen(cmd5, stdout=f, shell=False)
-		std_error = proc_5.communicate()
+	cmd5 = ['bedtools', 'intersect', '-v','-b', \
+		refpil, \
+		'-a', vcf_out, \
+		'-sorted', '-header']
+	with open(masked_vcf_out, "w+") as f:
+		proc_5 = subprocess.Popen(cmd5, stdout=f, shell=False)
+	std_error = proc_5.communicate()
 
 # Checks for dependencies required for snpEff.
-def snpeff(snpeffdb1, sortbam, bamboozledir1):
-	for bamfile in sortbam:
+def snpeff(snpeffdb1, bamfile, bamboozledir1):
 	#generating variables for function
-		bam_name = os.path.basename(bamfile[:-4])
-		masked_vcf_out = "sv_caller_output/%s_sorted_masked.vcf" % (bam_name)
-		masked_vcf_out_lof_csv = "sv_caller_output/%s_sorted_masked_lof.csv" % (bam_name)
-		masked_vcf_out_lof_ann = "sv_caller_output/%s_sorted_masked_lof.vcf" % (bam_name)
+	bam_name = os.path.basename(bamfile[:-4])
+	masked_vcf_out = "sv_caller_output/%s_sorted_masked.vcf" % (bam_name)
+	masked_vcf_out_lof_csv = "sv_caller_output/%s_sorted_masked_lof.csv" % (bam_name)
+	masked_vcf_out_lof_ann = "sv_caller_output/%s_sorted_masked_lof.vcf" % (bam_name)
 
-		cmd7 = ['snpEff', 'eff', snpeffdb1.replace("'", ""),\
-			 masked_ann_vcf_out, \
-			'-c', bamboozledir1+'/data/snpeff/snpEff.config', \
-			'-csvStats', masked_vcf_out_lof_csv]
-		with open(masked_vcf_out_lof_ann, "w+") as f:
-			proc_7 = subprocess.Popen(cmd7, stdout=f, shell=False)
-		std_error = proc_7.communicate()
+	cmd7 = ['snpEff', 'eff', snpeffdb1.replace("'", ""),\
+		 masked_ann_vcf_out, \
+		'-c', bamboozledir1+'/data/snpeff/snpEff.config', \
+		'-csvStats', masked_vcf_out_lof_csv]
+	with open(masked_vcf_out_lof_ann, "w+") as f:
+		proc_7 = subprocess.Popen(cmd7, stdout=f, shell=False)
+	std_error = proc_7.communicate()
 
 def main(args):
 	#picard java
@@ -152,13 +149,24 @@ def main(args):
 	ref_dict = "sv_caller_output/"+args.ref.replace(".fasta",".dict")
 	#clean database variable
 	snpeff_db = str(args.snpeffdb).strip('[]')
+
+#	print(args.sortbam)
 	
 	#calling functions for sv_caller
 	ref_check(args.ref, ref_dict)
-	run_gatk(args.sortbam, args.ref, java_picard)
-	#only apply masking() if it's been called
-	if args.masking:
-		masking(args.sortbam, args.masking)
-		snpeff(snpeff_db, args.sortbam, args.bamboozledir)
+	if len(args.sortbam) == 1:
+		run_gatk(args.sortbam, args.ref, java_picard)
+		if args.masking:
+			masking(args.sortbam, args.masking)
+			snpeff(snpeff_db, args.sortbam, args.bamboozledir)
+		else:
+			snpeff(snpeff_db, args.sortbam, args.bamboozledir)
 	else:
-		snpeff(snpeff_db, args.sortbam, args.bamboozledir)
+		for bamfile in args.sortbam:
+			#print(bamfile)
+			run_gatk(bamfile, args.ref, java_picard)
+			if args.masking:
+				masking(args.sortbam, args.masking)
+				snpeff(snpeff_db, args.sortbam, args.bamboozledir)
+			else:
+				snpeff(snpeff_db, args.sortbam, args.bamboozledir)
